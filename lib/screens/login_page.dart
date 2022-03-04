4484f8b05e01c6df7635829adf149fe0d5e03568
M A <ma.paracha@hotmail.com>
2022-03-05 02:07:39 +0500

    Added SinglechildScrollView to login and sign up page

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -27,103 +27,105 @@ class _LoginPageState extends State<LoginPage> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: false,
-      body: Column(
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Column(
 
-        children: [
-          // Head Image Container
-          AppHeadImage(),
-          SizedBox(height: w * 0.04,),
-          // Heading and textFields Container
-          Container(
+          children: [
+            // Head Image Container
+            AppHeadImage(),
+            SizedBox(height: w * 0.04,),
             // Heading and textFields Container
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            width: w,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Text
-                const AppText(text: 'Firebase Authentication App',
-                  fontSize: 30,
-                  fontWeight:
-                  FontWeight.bold,color: Colors.black54,),
-                AppText(text: 'Sign in to your account', fontSize: 20, color: Colors.green[400],),
+            Container(
+              // Heading and textFields Container
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              width: w,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Text
+                  const AppText(text: 'Firebase Authentication App',
+                    fontSize: 30,
+                    fontWeight:
+                    FontWeight.bold,color: Colors.black54,),
+                  AppText(text: 'Sign in to your account', fontSize: 20, color: Colors.green[400],),
 
-                 SizedBox(
-                  height: w * 0.08,
-                ),
-
-                // TextField Container
-                AppTextFields(hintText: "Email",
-                  prefixIcon: Icon(Icons.email, color: Colors.green[400],),
-                  controller: emailController,
-                ),
-
-                SizedBox(height: w * 0.04,),
-                AppTextFields(hintText: "Password",
-                  prefixIcon: Icon(Icons.password, color: Colors.green[400],)
-                  ,isPassField: true,
-                  controller: passwordController,
-                ),
-
-                SizedBox(height: w * 0.03,),
-                // Forgot Password Row
-                Row(
-                  // Forgot Password Row
-                  children:  [
-                    Spacer(),
-                    AppText(text: "Forgot your Password?",
-                      fontSize: 20,
-                      color: Colors.green[400],),
-                  ],
-                ),
-              ],
-            ),
-          ),
-
-          SizedBox(height: w * 0.1,),
-
-          // Sign In Button Container
-          GestureDetector(
-            onTap: () {
-              AuthController.instance.login(
-                  emailController.text.trim(), passwordController.text.trim());
-            },
-            child: const AppButton(btnText: 'Sign In',
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          SizedBox(
-            height: w * 0.08,
-          ),
-
-          // Register Account Text
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AppText(text: "Don\'t have an account? ",
-                  fontSize: 24,
-                  color: Colors.green[400],
-                ),
-
-                GestureDetector(
-                  onTap: (){
-                    Get.to(() => SignUpPage());
-                  },
-                  child: AppText(text: "Create",
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green[500],
+                   SizedBox(
+                    height: w * 0.08,
                   ),
-                ),
-              ],
-            ),
-          ),
 
-        ],
+                  // TextField Container
+                  AppTextFields(hintText: "Email",
+                    prefixIcon: Icon(Icons.email, color: Colors.green[400],),
+                    controller: emailController,
+                  ),
+
+                  SizedBox(height: w * 0.04,),
+                  AppTextFields(hintText: "Password",
+                    prefixIcon: Icon(Icons.password, color: Colors.green[400],)
+                    ,isPassField: true,
+                    controller: passwordController,
+                  ),
+
+                  SizedBox(height: w * 0.03,),
+                  // Forgot Password Row
+                  Row(
+                    // Forgot Password Row
+                    children:  [
+                      Spacer(),
+                      AppText(text: "Forgot your Password?",
+                        fontSize: 20,
+                        color: Colors.green[400],),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: w * 0.1,),
+
+            // Sign In Button Container
+            GestureDetector(
+              onTap: () {
+                AuthController.instance.login(
+                    emailController.text.trim(), passwordController.text.trim());
+              },
+              child: const AppButton(btnText: 'Sign In',
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: w * 0.08,
+            ),
+
+            // Register Account Text
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AppText(text: "Don\'t have an account? ",
+                    fontSize: 24,
+                    color: Colors.green[400],
+                  ),
+
+                  GestureDetector(
+                    onTap: (){
+                      Get.to(() => SignUpPage());
+                    },
+                    child: AppText(text: "Create",
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green[500],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   } // build
--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -29,129 +29,131 @@ class _SignUpPage extends State<SignUpPage> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        children: [
-          // Head Image and CircularAvatar Container
-          AppHeadImage(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: h * 0.17,
-                ),
-                const CircleAvatar(
-                  backgroundColor: Colors.white70,
-                  radius: 50,
-                  backgroundImage: AssetImage("img/profile.jpg"),
-                ),
-              ],
-            ),
-          ),
-
-          // Heading and textFields Container
-          Container(
-            // TextFields Container
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            width: w,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
-
-                // TextField Container
-                AppTextFields(
-                  hintText: "Email",
-                  prefixIcon: Icon(
-                    Icons.email,
-                    color: Colors.green[400],
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Head Image and CircularAvatar Container
+            AppHeadImage(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: h * 0.17,
                   ),
-                  controller: emailController,
-                ),
-
-                SizedBox(
-                  height: w * 0.04,
-                ),
-                AppTextFields(
-                  hintText: "Password",
-                  prefixIcon:  Icon(
-                    Icons.password,
-                    color: Colors.green[400],
+                  const CircleAvatar(
+                    backgroundColor: Colors.white70,
+                    radius: 50,
+                    backgroundImage: AssetImage("img/profile.jpg"),
                   ),
-                  isPassField: true,
-                  controller: passwordController,
-                ),
-                SizedBox(
-                  height: w * 0.04,
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
 
-          SizedBox(
-            height: 70,
-          ),
+            // Heading and textFields Container
+            Container(
+              // TextFields Container
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              width: w,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
 
-          // Sign Up Button Container
-          GestureDetector(
-            onTap: () {
-              AuthController.instance.register(
-                  emailController.text.trim(), passwordController.text.trim());
-            },
-            child: const AppButton(
-              btnText: 'Sign Up',
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+                  // TextField Container
+                  AppTextFields(
+                    hintText: "Email",
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.green[400],
+                    ),
+                    controller: emailController,
+                  ),
+
+                  SizedBox(
+                    height: w * 0.04,
+                  ),
+                  AppTextFields(
+                    hintText: "Password",
+                    prefixIcon:  Icon(
+                      Icons.password,
+                      color: Colors.green[400],
+                    ),
+                    isPassField: true,
+                    controller: passwordController,
+                  ),
+                  SizedBox(
+                    height: w * 0.04,
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: w * 0.02,
-          ),
 
-          // have ana account Text
-          GestureDetector(
-            onTap: () {
-              Get.back();
-            },
-            child: AppText(
-              text: "Have an account",
-              fontSize: 24,
+            SizedBox(
+              height: 70,
+            ),
+
+            // Sign Up Button Container
+            GestureDetector(
+              onTap: () {
+                AuthController.instance.register(
+                    emailController.text.trim(), passwordController.text.trim());
+              },
+              child: const AppButton(
+                btnText: 'Sign Up',
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: w * 0.02,
+            ),
+
+            // have ana account Text
+            GestureDetector(
+              onTap: () {
+                Get.back();
+              },
+              child: AppText(
+                text: "Have an account",
+                fontSize: 24,
+                color: Colors.green[400],
+              ),
+            ),
+
+            SizedBox(
+              height: w * 0.08,
+            ),
+
+            // SignUp method Text
+            AppText(
+              text: "Sign up using one of the following",
+              fontSize: 18,
               color: Colors.green[400],
             ),
-          ),
 
-          SizedBox(
-            height: w * 0.08,
-          ),
-
-          // SignUp method Text
-          AppText(
-            text: "Sign up using one of the following",
-            fontSize: 18,
-            color: Colors.green[400],
-          ),
-
-          // SignUP Methods Icons (Circle Images)
-          Wrap(
-              children: List<Widget>.generate(3, (index) {
-            return Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: CircleAvatar(
-                radius: 30,
-                backgroundColor: Colors.green[200],
+            // SignUP Methods Icons (Circle Images)
+            Wrap(
+                children: List<Widget>.generate(3, (index) {
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
                 child: CircleAvatar(
-                  radius: 25,
-                  backgroundImage: AssetImage(
-                    "img/" + images[index],
+                  radius: 30,
+                  backgroundColor: Colors.green[200],
+                  child: CircleAvatar(
+                    radius: 25,
+                    backgroundImage: AssetImage(
+                      "img/" + images[index],
+                    ),
                   ),
                 ),
-              ),
-            );
-          } // index return
-                  )),
-        ],
+              );
+            } // index return
+                    )),
+          ],
+        ),
       ),
     );
   } // build
